acff26506b06d088975b10da6fc72cc224a7f95f
krishnaveniabburi-arch <krishnaveni.abburi@gmail.com>
2026-06-25 13:36:50 -0400

    add new file

---
+++ index.js
@@ -0,0 +1,30 @@
+import 'dotenv/config';
+
+import express from "express";
+
+import connectDB from './db/conn.js'
+
+const PORT = 3000;
+const app = express();
+
+import grades from "./routes/grades.js";
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("result");
+});
+
+app.use("/grades", grades);
+
+// Global error handling
+app.use((err, _req, res, next) => {
+  res.status(500).send("Seems like we messed up somewhere...");
+});
+
+// Start the Express server
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
+
+connectDB();
